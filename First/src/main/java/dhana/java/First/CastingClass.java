1f7d00b6a2eed7c980d2f4dd0ab6df2ca096c47f
dhanasekardhaya <dhanasekardhaya@gmail.com>
2021-12-05 14:11:31 +0530

    casting

---
+++ First/src/main/java/dhana/java/First/CastingClass.java
@@ -0,0 +1,18 @@
+package dhana.java.First;
+
+public class CastingClass {
+	
+	public static void main(String[] hi)
+	{
+		// class name to casting
+		
+				Character s='A';
+				
+				Integer d=0;
+				
+				d=(int)s;
+				
+				System.out.println(d);
+	}
+
+}
